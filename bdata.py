8991641b78d8c228b65d562d4854cb8087395fb8
Bryan Bugyi (work) <bryanbugyi34@gmail.com>
2016-07-22 06:58:30 -0400

    bdata.py comments added

--- bdata.py
+++ bdata.py
@@ -1,9 +1,20 @@
+""" bdata.py
+
+All functions that relate directly to the user data should be stored in
+this module. """
+
 import pickle
 import payperiod
 import bdates
 
 
 def GetPayPeriod(date=None):
+    """ Used to return a payperiod object loaded with the pickle module
+
+    GetPayPeriod(date=None) -> PayPeriod Object
+
+    date = the start date of the payperiod that you would like to retrieve
+    """
     try:
         if not date:
             filename = bdates.getLatestPP() + '.db'
@@ -11,6 +22,9 @@ def GetPayPeriod(date=None):
             filename = date + '.db'
         with open('data/' + filename, 'rb') as F:
             PP = pickle.load(F)
+
+    # If the PayPeriod object could not be found, create a default PayPeriod
+    # object and save it to a file.
     except Exception as e:
         print(str(e))
         PP = payperiod.PayPeriod(1036.03, bdates.getLatestPP())
@@ -20,6 +34,12 @@ def GetPayPeriod(date=None):
 
 
 def SavePP(Obj):
+    """ Saves Obj to a file by using the pickle module to 'dump' it.
+
+    SavePP(Obj) -> None
+
+    Obj = The PayPeriod object that will be saved to a file.
+    """
     filename = Obj.StartDate + '.db'
     with open('data/' + filename, 'wb') as F:
         pickle.dump(Obj, F)
